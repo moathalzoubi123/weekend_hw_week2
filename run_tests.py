cea1ed8ffb9402a8e2ebc9f9b41ee57fd490e787
moath al-abdulrazaq <moath@moaths-MacBook-Air.local>
2022-05-29 21:24:50 +0100

    3rd commad

--- run_tests.py
+++ run_tests.py
@@ -2,7 +2,6 @@ import unittest
 
 from tests.guests_tests import TestGuest
 from tests.rooms_tests import TestRoom 
-from tests.karaoke_tests import TestKaraoke
 
 
 
--- src/karaoke.py
+++
@@ -1,4 +0,0 @@
-class Karaoke:
-    def __init__(self, name,):
-        self.name = name 
-        self.room = {}
